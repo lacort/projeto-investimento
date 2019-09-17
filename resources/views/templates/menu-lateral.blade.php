5372ed4f0037a3faa1176567dd63119616aa1241
lacort <lacort2@gmail.com>
2019-09-17 13:06:15 -0300

    CSS menu lateral parte 1

---
+++ resources/views/templates/menu-lateral.blade.php
@@ -0,0 +1,25 @@
+<nav id ="principal">
+    <ul>
+        <li>
+            <a href="">
+                <i class="" ></i>
+                <h3>Usuários</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="" ></i>
+                <h3>Instituições</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="" ></i>
+                <h3>Grupos</h3>
+            </a>
+        </li>
+    </ul>
+
+
+</nav>
+
